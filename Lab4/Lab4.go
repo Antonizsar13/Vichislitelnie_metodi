32b1716b9f1447002fa08702a794dfb31fad7c41
antonizsar13 <antonizsar13@gmail.com>
2024-09-27 10:12:06 +0300

    1234

---
+++ Lab4/Lab4.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type element struct {
+	number float64
+	degree int
+}
+
+func enterEquations() {
+	var size int
+	fmt.Print("Введите количество элементов:")
+	fmt.Scanf("%d", &size)
+
+	
+	for i := 0; i < size; i++ {
+
+	}
+
+	// return []element;
+}
+
+func main() {
+	enterEquations()
+
+	// print(equations)
+}
