d36ed6d81b4d89b961b2fc2adbfb30acc8169aa6
Your Nameantonizsar13 <Antonizsar13@gmail.com>
2024-09-28 15:18:53 +0300

    lab 4

--- Lab4/Lab4.go
+++ Lab4/Lab4.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-    "math"
+	"math"
 )
 
 type Element struct {
@@ -11,18 +11,22 @@ type Element struct {
 }
 
 func enterEquations() []Element {
-	var size int
-	fmt.Print("Введите количество элементов:")
-	fmt.Scanf("%d", &size)
+	// var size int
+	// fmt.Print("Введите количество элементов:")
+	// fmt.Scanf("%d", &size)
 
-	elements := make([]Element, size)
-	fmt.Println("Введите элементы:")
-	for i := 0; i < size; i++ {
-		fmt.Printf("Число %d: ", i)
-		fmt.Scan(&elements[i].number)
-		fmt.Printf("Степерь числа %d: ", i)
-		fmt.Scan(&elements[i].degree)
-	}
+	// elements := make([]Element, size)
+	// fmt.Println("Введите элементы:")
+	// for i := 0; i < size; i++ {
+	// 	fmt.Printf("Число %d: ", i)
+	// 	fmt.Scan(&elements[i].number)
+	// 	fmt.Printf("Степерь числа %d: ", i)
+	// 	fmt.Scan(&elements[i].degree)
+	// }
+
+	elements := []Element{{1, 3},
+		{4, 1},
+		{-3, 0}}
 
 	return elements
 }
@@ -40,30 +44,32 @@ func printEquations(equations []Element) {
 }
 
 func derivative(equations []Element) []Element {
-	for i, element := range equations {
+	equationsNew := make([]Element, len(equations))
+	copy(equationsNew, equations)
+	for i, element := range equationsNew {
 		if element.degree == 0 {
-			copy(equations[i:], equations[i+1:])
-			equations = equations[:len(equations)-1]
+			copy(equationsNew[i:], equationsNew[i+1:])
+			equationsNew = equationsNew[:len(equationsNew)-1]
 		} else {
-			equations[i].number *= float64(element.degree)
-			equations[i].degree--
+			equationsNew[i].number *= float64(element.degree)
+			equationsNew[i].degree--
 		}
 	}
-	return equations
+	return equationsNew
 }
 
 func calculateFunction(Xn float64, equations []Element) float64 {
-	var answerd float64;
+	var answerd float64
 
-	for i, element := range equations {
+	for _, element := range equations {
 		answerd += (element.number * (math.Pow(Xn, float64(element.degree))))
 	}
-	
+
 	return answerd
 }
 
 func main() {
-	var Xn float64 = 2
+	var Xn float64 = 1
 	var E float64 = 0.001
 
 	equations := enterEquations()
@@ -71,11 +77,21 @@ func main() {
 
 	derivativeEq := derivative(equations)
 	printEquations(derivativeEq)
-
 	for i := Xn; i <= 1024; i *= 2 {
-		var Xn2 float32 = i;
-			fmt.Println("\nНачальное приближение = %f", Xn2)
-		for Xn2 
+		var Xn2 float64 = i
+		fmt.Printf("\nНачальное приближение = %f\n", Xn2)
+		fmt.Println("X\t| F(Xn)\t| F′(Xn)\t| diff")
+		var different float64 = E
+		for math.Abs(different) >= E {
+
+			answerd1 := calculateFunction(Xn2, equations)
+			answerd2 := calculateFunction(Xn2, derivativeEq)
+
+			different = answerd1 / answerd2
+
+			Xn2 = Xn2 - different
+			fmt.Printf("%f\t| %f\t| %f\t| %f\n", Xn2, answerd1, answerd2, different)
+		}
 
 	}
 
